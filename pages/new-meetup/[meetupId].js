b63bb4877c6166cf37527b0f6ed78ed0969477b1
xXnielsXx <nielsmichael@comcast.net>
2022-07-10 19:22:31 +0900

    Add MeetupDetail component to pass props, w/ styling

---
+++ pages/new-meetup/[meetupId].js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail 
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Scintillant_hummingbird_%28Selasphorus_scintilla%29_female_in_flight_1.jpg/1280px-Scintillant_hummingbird_%28Selasphorus_scintilla%29_female_in_flight_1.jpg"
+      alt='A first meetup' 
+      address='Some address'
+      description='Meetup description'
+     />
+  )
+}
+
+export default MeetupDetails
