7ebd0dd8653b393576fba5ed47264913e481c586
xXnielsXx <nielsmichael@comcast.net>
2022-07-13 11:09:36 +0900

    Add new-meetup backend api call to new-meetup page

--- pages/new-meetup.js
+++ pages/new-meetup.js
@@ -1,8 +1,27 @@
+import { useRouter } from 'next/router'
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 const NewMeetup = () => {
-  const addMeetupHandler = enteredMeetupData => {
-    console.log(enteredMeetupData)
+  const router = useRouter()
+
+  const addMeetupHandler = async enteredMeetupData => {
+    // Send request to /api/new-meetup
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const data = await response.json()
+
+    console.log(data)
+
+    // Redirect with router.push()
+    router.push('/')
+    // Can also use router.replace() to ensure that page cannot be reloadable
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />
